a987403dfa1c9170e858b45b4e5ae36b089f1209
rezaulkhan111 <rezaulkhan111@hotmail.com>
2024-07-16 10:17:58 +0600

    commit 1st

---
+++ BDHighwayWebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BDHighwayWebSite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
